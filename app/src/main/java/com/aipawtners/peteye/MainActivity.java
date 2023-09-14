637a8f3d17858d0e5e0d77c888a611a8cec8cd15
ihb0301 <abin0301@naver.com>
2023-09-15 01:23:09 +0900

    하단 네비게이션바 추가

--- app/src/main/java/com/aipawtners/peteye/MainActivity.java
+++ app/src/main/java/com/aipawtners/peteye/MainActivity.java
@@ -1,14 +1,44 @@
 package com.aipawtners.peteye;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainFragment main_fragment;
+    InfoFragment info_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_fragment=new MainFragment();
+        info_fragment=new InfoFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,main_fragment).commit();
+
+        BottomNavigationView bottomNavigation=findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener(){
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        if(item.getItemId()==R.id.tab_main){
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container, main_fragment).commit();
+                            return true;
+                        }else if(item.getItemId()==R.id.tab_information){
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container, info_fragment).commit();
+                            return true;
+                        }
+                        return false;
+            }
+        }
+        );
     }
 }
